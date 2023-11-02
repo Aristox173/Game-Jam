ba24d7d1b0d55597692232f58b7e61cae4bdf185
Juan Aristizábal <juanaristi2001@gmail.com>
2023-11-01 20:20:45 -0500

    Add Camera Limits

--- Assets/Player/Scripts/Player.cs
+++ Assets/Player/Scripts/Player.cs
@@ -8,6 +8,17 @@ public class Player : MonoBehaviour
     public float acceleration = 5.0f;
     public float maxSpeed = 10.0f;
 
+    private Camera mainCamera;
+    private float cameraWidth;
+    private float cameraHeight;
+
+    private void Start()
+    {
+        mainCamera = Camera.main;
+        cameraHeight = mainCamera.orthographicSize;
+        cameraWidth = cameraHeight * mainCamera.aspect;
+    }
+
     private void Update()
     {
         // Rotation input
@@ -23,6 +34,12 @@ public class Player : MonoBehaviour
         float moveSpeed = moveInput * acceleration;
         Vector3 moveDirection = transform.up * moveSpeed;
         transform.Translate(moveDirection * Time.deltaTime, Space.World);
+
+        // Clamp the spaceship's position within camera bounds
+        Vector3 clampedPosition = transform.position;
+        clampedPosition.x = Mathf.Clamp(clampedPosition.x, -cameraWidth + 0.5f, cameraWidth - 0.5f);
+        clampedPosition.y = Mathf.Clamp(clampedPosition.y, -cameraHeight + 0.5f, cameraHeight - 0.5f);
+        transform.position = clampedPosition;
 
         // Limit the speed of the spaceship
         if (moveDirection.magnitude > maxSpeed)
